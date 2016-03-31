ccb916ecd66d48daae3b10b4f83d347e0ec99832
thangdt <thangdt.coltech@gmail.com>
2016-03-31 16:52:11 +0700

    props and state

--- App.js
+++ App.js
@@ -1,9 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      title: 'This is content'
+    };
+  }
+  updateTitle(e){
+    this.setState({title: e.target.value});
+  }
+  updateTitleByClass(e){
+    var newTitle = ReactDOM.findDOMNode(this.refs.title).value;
+    this.setState({title: newTitle});
+  }
+  render(){
+    let title = this.state.title;
+    let content = this.props.content;
+    let updateTitle = this.updateTitle.bind(this);
+
     return (
-      <div>Hello</div>
+      <div>
+        <Widget title={title} content={content} updateTitle={updateTitle}/>
+        <Silder ref="title" updateTitleByClass={this.updateTitleByClass}/>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  content: React.PropTypes.string
+};
+
+App.defaultProps = {
+  content: 'Content'
+};
+
+const Widget = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <input type="text" onChange={props.updateTitle} />
+    </div>
+  );
+};
+
+class Silder extends React.Component() {
+  render(){
+    return (
+      <input type="range" min="0" max="100" onChange={this.props.updateTitleByClass} />
     );
   }
 }
